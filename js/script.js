8d1fd4a3e9d84c68c4a4ea7291c92003fe9abbcb
florianalauro <floriana.lauro@gmail.com>
2026-02-02 15:37:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -94,7 +94,7 @@ function createSwarm(swarmContainer) {
     
     butterfly.setAttribute('gltf-model', '#butterflyModel');
     butterfly.setAttribute('animation-mixer', 'clip: Flying');
-    butterfly.setAttribute('scale', '0.15 0.10 0.15');
+    butterfly.setAttribute('scale', '0.2 0.15 0.2');
     butterfly.setAttribute('butterfly-color', 'color: #ce0058');
 
     const resetButterfly = (el) => {
